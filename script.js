00864ef9a39b6ccba07ceef66a57841125edf139
mariaafer057 <mariafernandaalves057@gmail.com>
2023-05-02 14:19:58 -0400

    commit inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+//empresaProduto
+class Produto{
+    constructor (Nome, DataCadastro, Descricao, Preco){
+    this.Nome = Nome;
+    this.DataCadastro = DataCadastro;
+    this.Descricao = Descricao;
+    this.Preco = Preco;
+}
+Mostra_produtos(){
+    return this.Nome + this.DataCadastro + this.Descricao + this.Preco
+    }
+}
+class ProdutoDestaque extends Produto{
+constructor (Nome, DataCadastro, Descricao, Preco, imagemDestaque){
+super(Nome, DataCadastro, Descricao, Preco)
+this.imagemDestaque = imagemDestaque;
+}
+Mostra_produtosDestaque(){
+    return this.Nome + this.DataCadastro + this.Descricao + this.Preco + this.imagemDestaque
+    }
+}
+let produto = new Produto("Celular", "lindo", "Funcionavel" , 999)
+console.log(produto.Mostra_produtos())
+
+let produtodestaque = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 999, "www.imagemdecelular.com")
+console.log(produtodestaque.Mostra_produtosDestaque())
