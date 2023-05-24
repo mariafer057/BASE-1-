77d5397ca4be0ad7e7177693aac19976ebac8028
Luiz F. Picolo <luizpicolo@gmail.com>
2023-05-24 13:56:08 -0400

    atualizado2

--- script.js
+++ script.js
@@ -39,35 +39,35 @@ div.insertAdjacentHTML('afterbegin', produtodestaque.mostrar_produtos_destaque()
 
 let produtodestaque1 = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 7899.5, "https://m.media-amazon.com/images/I/71ZDY57yTQL._AC_SX522_.jpg")
 console.log(produtodestaque1.mostrar_produtos_destaque())
-const div1 = document.getElementById("produto-destaque");
+const div1 = document.getElementById("lista-produtos");
 div1.insertAdjacentHTML('afterbegin', produtodestaque1.mostrar_produtos_destaque())
 
 let produtodestaque2 = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 6299.5, "https://m.media-amazon.com/images/I/51yacbtHFuL._AC_SX679_.jpg")
 console.log(produtodestaque2.mostrar_produtos_destaque())
-const div2 = document.getElementById("produto-destaque");
+const div2 = document.getElementById("lista-produtos");
 div2.insertAdjacentHTML('afterbegin', produtodestaque2.mostrar_produtos_destaque())
 
 let produtodestaque3 = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 1299.5, "https://m.media-amazon.com/images/I/51uD1lmrV8L._AC_SX679_.jpg")
 console.log(produtodestaque3.mostrar_produtos_destaque())
-const div3 = document.getElementById("produto-destaque");
+const div3 = document.getElementById("lista-produtos");
 div3.insertAdjacentHTML('afterbegin', produtodestaque3.mostrar_produtos_destaque())
 
 let produtodestaque4 = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 4299.5, "https://m.media-amazon.com/images/I/61BGE6iu4AL._AC_SX679_.jpg")
 console.log(produtodestaque4.mostrar_produtos_destaque())
-const div4 = document.getElementById("produto-destaque");
+const div4 = document.getElementById("lista-produtos");
 div4.insertAdjacentHTML('afterbegin', produtodestaque4.mostrar_produtos_destaque())
 
 let produtodestaque5 = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 8299.5, "https://m.media-amazon.com/images/I/715HCLsOHbL._AC_SX679_.jpg ")
 console.log(produtodestaque5.mostrar_produtos_destaque())
-const div5 = document.getElementById("produto-destaque");
+const div5 = document.getElementById("lista-produtos");
 div5.insertAdjacentHTML('afterbegin', produtodestaque5.mostrar_produtos_destaque())
 
 let produtodestaque6 = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 8299.6, "https://m.media-amazon.com/images/I/51misXwIxAL._AC_SX679_.jpg")
 console.log(produtodestaque6.mostrar_produtos_destaque())
-const div6 = document.getElementById("produto-destaque");
+const div6 = document.getElementById("lista-produtos");
 div6.insertAdjacentHTML('afterbegin', produtodestaque6.mostrar_produtos_destaque())
 
 let produtodestaque7 = new ProdutoDestaque("Celular", "lindo", "Funcionavel" , 8299.6, "https://m.media-amazon.com/images/I/71iO2R+CLjL._AC_SX679_.jpg")
 console.log(produtodestaque7.mostrar_produtos_destaque())
-const div7 = document.getElementById("produto-destaque");
+const div7 = document.getElementById("lista-produtos");
 div7.insertAdjacentHTML('afterbegin', produtodestaque7.mostrar_produtos_destaque())
